d84bf90986ccd8191d5b34fedca019a519db8263
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-07 10:56:49 +0500

    Solution

--- 1343.Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.js
+++ 1343.Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.js
@@ -1,3 +1,33 @@
 // Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k and average greater than or equal to threshold.
 
- 
+ /**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} threshold
+ * @return {number}
+ */
+var numOfSubarrays = function(arr, k, threshold) {
+    
+    let start = 0
+    let end = 0
+    let total = 0
+    let sum = 0
+
+
+    while(end<arr.length){
+        sum = sum + arr[end]
+        if(end- start + 1 >= k){
+    
+        let avg = sum / k 
+        if(avg >= threshold)  {          
+            total++
+        }
+        let first = arr[start]
+        sum =  sum - first
+        start++
+    }
+    end ++
+    }
+    return total
+
+};
