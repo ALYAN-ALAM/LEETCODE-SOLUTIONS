0d528c156e31599b9c185fa92dc492efa482ab4e
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-07 10:54:48 +0500

    Solution

--- 904.Fruit-Into-Baskets.js
+++ 904.Fruit-Into-Baskets.js
@@ -6,3 +6,59 @@
 // Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
 // Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
 // Given the integer array fruits, return the maximum number of fruits you can pick.
+
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function (fruits) {
+  let windowStart = 0;
+  let windowEnd = 0;
+  let maxFruits = 0;
+  let CurrMax = 0;
+
+  let fruitCount = new Map();
+
+  while (windowEnd < fruits.length) {
+    curr = fruits[windowEnd];
+    if (fruitCount.size < 2) {
+      if (fruitCount.has(curr)) {
+        fruitCount.set(curr, fruitCount.get(curr) + 1);
+        CurrMax++;
+      } else {
+        fruitCount.set(curr, 1);
+        CurrMax++;
+      }
+      windowEnd++;
+      if (CurrMax > maxFruits) {
+        maxFruits = CurrMax;
+      }
+    } else if (fruitCount.size == 2) {
+      if (fruitCount.has(curr)) {
+        fruitCount.set(curr, fruitCount.get(curr) + 1);
+        CurrMax++;
+      } else {
+        fruitCount.set(curr, 1);
+        //   CurrMax--
+      }
+      if (CurrMax > maxFruits) {
+        maxFruits = CurrMax;
+      }
+      windowEnd++;
+    } else {
+      let first = fruits[windowStart];
+      if (fruitCount.has(first)) {
+        if (fruitCount.get(first) <= 1) {
+          fruitCount.delete(first);
+          // CurrMax--
+        } else {
+          fruitCount.set(first, fruitCount.get(first) - 1);
+          CurrMax--;
+        }
+      }
+      windowStart++;
+    }
+  }
+
+  return maxFruits;
+};
