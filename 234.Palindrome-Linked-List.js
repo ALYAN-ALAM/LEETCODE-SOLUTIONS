f7f675d1dfc63bfff26d790c36ede1029e34015e
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-09 21:26:15 +0500

    solution

--- 234.Palindrome-Linked-List.js
+++ 234.Palindrome-Linked-List.js
@@ -1,2 +1,52 @@
 // Given the head of a singly linked list, return true if it is a palindrome
 //  or false otherwise.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const reverse = (head) => {
+    let curr = head
+    let prev = null
+    let next
+
+    while(curr!==null){
+        next = curr.next
+        curr.next = prev
+        prev = curr 
+        curr = next
+    }
+    return prev
+ }
+
+var isPalindrome = function(head) {
+    let fast = head
+    let slow = head
+    let startPointer =  head
+    let len = 0
+
+    while(fast && fast.next ){
+        slow = slow.next
+        fast = fast.next.next
+        len++
+    }
+
+    
+
+    let mid = reverse(slow)
+
+    while(len > 0){
+        len--
+        if(mid.val !== startPointer.val) return false
+        mid = mid.next
+        startPointer = startPointer.next
+    }
+    return true
+};
