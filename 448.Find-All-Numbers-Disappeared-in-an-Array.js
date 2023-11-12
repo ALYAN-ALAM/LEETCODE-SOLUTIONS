3cdc0fb4f828fa0336e62be1977ccd840571695a
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-12 10:59:22 +0500

    solution

--- 448.Find-All-Numbers-Disappeared-in-an-Array.js
+++ 448.Find-All-Numbers-Disappeared-in-an-Array.js
@@ -2,3 +2,26 @@
 
  
 
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let result = [];
+    let i = 0
+    while(i<nums.length){
+        if(nums[i]!=i+1 && nums[i]!=nums[nums[i]-1]){
+            [nums[nums[i]-1],nums[i]] = [nums[i],nums[nums[i]-1]]
+        }
+        else{
+            i++
+        }
+    }
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!=i+1){
+            result.push(i+1)
+        }
+    }
+    return result
+
+};
