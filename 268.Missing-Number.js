1cd4aad9f691fed2423718ce6d19a752ed3fa47c
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-12 10:58:00 +0500

    Solution

--- 268.Missing-Number.js
+++ 268.Missing-Number.js
@@ -1,1 +1,27 @@
 // Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let i =0
+    while(i<nums.length){
+
+        if(nums[i]<nums.length && nums[i]!=i){
+            [nums[nums[i]],nums[i]] = [nums[i],nums[nums[i]]]
+        }
+        else{
+            i++
+        }
+
+    }
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!=i){
+            return i
+        }
+    }
+
+    return nums.length
+    
+};
