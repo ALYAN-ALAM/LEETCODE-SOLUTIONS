f68ebb59c0db08e82e99d6094ce0dcb70ce0eab0
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-15 13:16:57 +0500

    Solution

--- 1290.Convert-Binary-Number-in-a-Linked-List-to-Integer.js
+++ 1290.Convert-Binary-Number-in-a-Linked-List-to-Integer.js
@@ -4,4 +4,37 @@
 
 // The most significant bit is at the head of the linked list.
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+
+    let decimal = 0
+    let curr = head
+    let prev = null
+    while(curr!=null){
+        let nextnum = curr.next
+        curr.next = prev
+        prev= curr
+        curr = nextnum
+    }
+    curr = prev
+    // prev = null
+    let i = 0
+    while(curr!=null){
+        decimal += curr.val * Math.pow(2, i)
+        curr = curr.next
+        i++
+    }
+    return decimal
+};
+
  
