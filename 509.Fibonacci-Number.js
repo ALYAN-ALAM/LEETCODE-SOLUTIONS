21aa4ce73332322bd8e33876c8b0e0149dfcf2a5
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-18 16:59:40 +0500

    Solution

--- 509.Fibonacci-Number.js
+++ 509.Fibonacci-Number.js
@@ -3,3 +3,16 @@
 // F(0) = 0, F(1) = 1
 // F(n) = F(n - 1) + F(n - 2), for n > 1.
 // Given n, calculate F(n).
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+
+var fib = function(n) {
+    if (n==0) return 0
+    if (n==1) return 1
+    let total = fib(n-1) + fib(n-2)  
+    return total
+  };
