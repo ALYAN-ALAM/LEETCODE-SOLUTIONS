e1a04466e84159cf5dc2bc96e83fb9a1ec0b5118
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-21 22:55:07 +0500

    Solution

--- 22.Generate-Parentheses.js
+++ 22.Generate-Parentheses.js
@@ -1,1 +1,27 @@
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = []
+    findAll("(",1,0,result,n)
+    return result
+    
+};
+
+var findAll = function(curr,openNum,closeNum,result,n) {
+    if(curr.length == n*2){
+        result.push(curr)
+        return
+    }
+    if(openNum<n){
+       findAll(curr+"(",openNum+1,closeNum,result,n) 
+    }
+    if(closeNum<openNum){
+        findAll(curr+")",openNum,closeNum+1,result,n) 
+    }
+
+
+}
