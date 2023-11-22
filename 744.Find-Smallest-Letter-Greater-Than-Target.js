da93fd3c655f37d361a9d31cbc2ce094907e9e6c
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-22 12:01:08 +0500

    Solution

--- 744.Find-Smallest-Letter-Greater-Than-Target.js
+++ 744.Find-Smallest-Letter-Greater-Than-Target.js
@@ -1,3 +1,27 @@
 // You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 
 // Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    let start = 0
+    let end = letters.length - 1 
+  
+     while(start<=end){
+        let middle = start + Math.floor((end - start)/2)
+         
+        if(target<letters[middle]){
+           end = middle - 1
+        }
+        else{
+           start =middle + 1
+        }
+        
+     }
+  
+     return letters[start % letters.length]
+  }
