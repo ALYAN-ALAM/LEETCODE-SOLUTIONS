ffea6768747875db63c52f31155009434aeb59be
ALYAN ALAM <alyanalam780@gmail.com>
2023-11-30 00:00:10 +0500

    Solution

--- 129.Sum-Root-to-Leaf-Numbers.js
+++ 129.Sum-Root-to-Leaf-Numbers.js
@@ -6,3 +6,29 @@
 // Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
 
 // A leaf node is a node with no children.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ 
+var sumNumbers = function(root,currentSum = 0) {
+    if (root == null){
+        return 0
+    }
+    currentSum = currentSum * 10 + root.val
+    if(root.left == null && root.right == null){
+        return currentSum
+    }
+
+    return sumNumbers(root.left,currentSum) + sumNumbers(root.right,currentSum)
+    
+};
