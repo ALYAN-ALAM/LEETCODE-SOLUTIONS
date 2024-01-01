5117f10d4a1c19189dc45e9536a6e4474dc57353
ALYAN ALAM <alyanalam780@gmail.com>
2024-01-01 23:36:37 +0500

    78 Solution

--- 78.Subsets.js
+++ 78.Subsets.js
@@ -4,3 +4,28 @@
 
 // The solution set must not contain duplicate subsets. Return the solution in any order.
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    // let mySet = new Set();
+ let arr = []
+ arr.push([]);
+
+ for(let i=0;i<nums.length;i++){
+     let arrLen = arr.length;
+     for(let j=0;j<arrLen;j++){
+         let mySet = arr[j].slice()
+         // mySet.add(nums[i])
+         mySet.push(nums[i])
+        
+         arr.push(mySet)
+         
+
+     }
+ }
+
+ return arr
+
+};
